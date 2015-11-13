1b8f632598c49239185505e7c8d6fb5e8639fb7b
Brandon Klimek <sixthpoint@gmail.com>
2015-11-12 21:52:29 -0600

    added bootstrap

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 // Require all css
-require("bootstrap-webpack");
+//require("bootstrap-webpack");
+require("../css/css/bootstrap.css");
 require("../css/styles.css");
+
+// BootstrapJS
+require("../js/bootstrap/bootstrap.js");
 
 // Load backboneJS router
 var Workspace = require("./router");
