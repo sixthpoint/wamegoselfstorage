84be7ca341a3523522e4a8ebc3519aedbc37f071
Brandon Klimek <sixthpoint@gmail.com>
2015-12-19 09:45:10 -0600

    added validation rules

--- js/project/models/RegistrationModel.js
+++ js/project/models/RegistrationModel.js
@@ -15,10 +15,13 @@ var RegistrationModel = Backbone.Model.extend({
             required: true
         },
         state: {
-            required: true
+            required: true,
+            oneOf: ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
         },
         email: {
-            required: true
+            required: true,
+            pattern: 'email',
+            msg: "Valid email address is required"
         },
         phone: {
             required: true
